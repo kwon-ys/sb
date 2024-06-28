67264c5381cdddfe6a0e40260661794a64704a33
kwon-ys <202003339@g.seoil.ac.kr>
2024-06-28 09:37:18 +0900

    update

--- sbtest/src/test/java/com/kys/sbtest/PhoneBookRepositoryTest.java
+++ sbtest/src/test/java/com/kys/sbtest/PhoneBookRepositoryTest.java
@@ -13,47 +13,48 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PhoneBookRepositoryTest {
     @Test
     public void jsonRepositoryTest() throws Exception {
-        PhoneBookJSONRepository repository = new PhoneBookJSONRepository("test.json");
-        String json = "{\"phoneNumber\":\"010-0000-0000\",\"name\":\"이말자\",\"id\":7,\"email\":\"asdfs@gm.com\",\"group\":\"Jobs\"}";
+        PhoneBookJSONRepository repository = new PhoneBookJSONRepository("test111.json");
+        String json = "{\"phoneNumber\":\"010-1234-4321\",\"name\":\"이말년\",\"id\":55,\"email\":\"chim@gg.com\",\"group\":\"Families\"}";
         JSONParser jsonParser = new JSONParser();
         IPhoneBook phoneBook = null;
         Object obj = jsonParser.parse(json);
         phoneBook = repository.getObjectFromJson((JSONObject)obj);
-        assertThat(phoneBook.getId()).isEqualTo(7L);
-        assertThat(phoneBook.getName()).isEqualTo("이말자");
-        assertThat(phoneBook.getGroup()).isEqualTo(EPhoneGroup.Jobs);
-        assertThat(phoneBook.getPhoneNumber()).isEqualTo("010-0000-0000");
-        assertThat(phoneBook.getEmail()).isEqualTo("asdfs@gm.com");
+        assertThat(phoneBook.getId()).isEqualTo(55L);
+        assertThat(phoneBook.getName()).isEqualTo("이말년");
+        assertThat(phoneBook.getGroup()).isEqualTo(EPhoneGroup.Families);
+        assertThat(phoneBook.getPhoneNumber()).isEqualTo("010-1234-4321");
+        assertThat(phoneBook.getEmail()).isEqualTo("chim@gg.com");
 
         IPhoneBook phoneBook2 = new PhoneBook();
-        phoneBook2.setId(88L);
-        phoneBook2.setName("폰북");
-        phoneBook2.setGroup(EPhoneGroup.Hobbies);
-        phoneBook2.setPhoneNumber("1111-2222");
-        phoneBook2.setEmail("abcdefg@daum.net");
+        phoneBook2.setId(null);
+        phoneBook2.setName("북북북");
+        phoneBook2.setGroup(EPhoneGroup.Schools);
+        phoneBook2.setPhoneNumber("0101010-0101010");
+        phoneBook2.setEmail("qwerasdf@grgr.net");
         JSONObject jobject = repository.getJsonFromObject(phoneBook2);
-        assertThat((Long)jobject.get("id")).isEqualTo(88L);
-        assertThat((String)jobject.get("name")).isEqualTo("폰북");
-        assertThat((String)jobject.get("group")).isEqualTo("Hobbies");
-        assertThat((String)jobject.get("phoneNumber")).isEqualTo("1111-2222");
-        assertThat((String)jobject.get("email")).isEqualTo("abcdefg@daum.net");
-        assertThat(jobject.toJSONString()).isEqualTo("{\"phoneNumber\":\"1111-2222\",\"name\":\"폰북\",\"id\":88,\"email\":\"abcdefg@daum.net\",\"group\":\"Hobbies\"}");
+        assertThat((Long)jobject.get("id")).isEqualTo(null);
+        assertThat((String)jobject.get("name")).isEqualTo("북북북");
+        assertThat((String)jobject.get("group")).isEqualTo("Schools");
+        assertThat((String)jobject.get("phoneNumber")).isEqualTo("0101010-0101010");
+        assertThat((String)jobject.get("email")).isEqualTo("qwerasdf@grgr.net");
+        assertThat(jobject.toJSONString()).isEqualTo(
+                "{\"phoneNumber\":\"0101010-0101010\",\"name\":\"북북북\",\"id\":null,\"email\":\"qwerasdf@grgr.net\",\"group\":\"Schools\"}");
     }
 
     @Test
     public void textRepositoryTest() throws Exception {
-        PhoneBookTextRepository repository = new PhoneBookTextRepository("test.json");
+        PhoneBookTextRepository repository = new PhoneBookTextRepository("test222.json");
         Throwable ex = assertThrows(Exception.class, () -> repository.getObjectFromText(""));
         System.out.println(ex.toString());
 
-        IPhoneBook phoneBook = repository.getObjectFromText("3,홍길동,Families,010-1111-1111,abcd@gmail.com");
-        assertThat(phoneBook.getId()).isEqualTo(3L);
-        assertThat(phoneBook.getName()).isEqualTo("홍길동");
-        assertThat(phoneBook.getGroup()).isEqualTo(EPhoneGroup.Families);
+        IPhoneBook phoneBook = repository.getObjectFromText("123,길길동,Friends,010-1111-1111,1234567@com.com");
+        assertThat(phoneBook.getId()).isEqualTo(123L);
+        assertThat(phoneBook.getName()).isEqualTo("길길동");
+        assertThat(phoneBook.getGroup()).isEqualTo(EPhoneGroup.Friends);
         assertThat(phoneBook.getPhoneNumber()).isEqualTo("010-1111-1111");
-        assertThat(phoneBook.getEmail()).isEqualTo("abcd@gmail.com");
+        assertThat(phoneBook.getEmail()).isEqualTo("1234567@com.com");
 
         String str = repository.getTextFromObject(phoneBook);
-        assertThat(str).isEqualTo("3,홍길동,Families,010-1111-1111,abcd@gmail.com\n");
+        assertThat(str).isEqualTo("123,길길동,Friends,010-1111-1111,1234567@com.com\n");
     }
 }
